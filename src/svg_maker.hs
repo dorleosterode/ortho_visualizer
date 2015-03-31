17bc5a0f67c10e588de14f045f2c98c59ed7ef22
Dorle Osterode <9osterod@informatik.uni-hamburg.de>
2015-03-31 12:32:28 +0200

    changed the ordering of functions. added comments

--- src/svg_maker.hs
+++ src/svg_maker.hs
@@ -6,6 +6,7 @@ import qualified Text.Blaze.Svg11 as S
 import qualified Text.Blaze.Svg11.Attributes as A
 import Text.Blaze.Svg.Renderer.String (renderSvg)
 
+-- main functions
 main :: IO ()
 main = do
   let a = renderSvg svgDoc
@@ -17,11 +18,13 @@ svgDoc = S.docTypeSvg ! A.version "1.1" ! A.width "300" ! A.height "300" ! A.vie
   makeCDSs lst
   connectCDSs clst
 
+-- example contigs and lists
 c1 = (1, 5, 0, True)
 c2 = (1, 5, 10, False)
 lst = [c1, c2]
 clst = [(c1, c2, False)]
 
+-- helper functions for tuple
 first4 (x, _, _, _) = x
 second4 (_, y, _, _) = y
 third4 (_, _, z, _) = z
@@ -31,11 +34,30 @@ first3 (x, _, _) = x
 second3 (_, y, _) = y
 third3 (_, _, z) = z
 
+-- example function to transform the whole image
+-- makeTransform :: S.AttributeValue
+-- makeTransform = rotate 0
+
+-- functions to draw contigs
 makeCDSs :: [(Int,Int,Int,Bool)] -> S.Svg
 makeCDSs [x] = makeCDS (first4 x) (second4 x) (third4 x) (forth4 x)
 makeCDSs (x:xs) = do makeCDS (first4 x) (second4 x) (third4 x) (forth4 x)
                      makeCDSs xs
 
+makeCDS :: Int -> Int -> Int -> Bool -> S.Svg
+makeCDS y l s sense = S.polygon !
+  A.fill "#008d46" !
+  A.stroke "#000000" !
+  A.strokeWidth "0.2" !
+  (A.points $ toValue $ if sense then formatRPolyP y l s else formatLPolyP y l s)
+
+formatLPolyP :: Int -> Int -> Int -> String
+formatLPolyP y l s = printf "%d,%d %d,%d %d,%d %d,%d %d,%d" s (y + 1) (s + 1) y (s + l + 1) y (s + l + 1) (y + 2) (s + 1) (y + 2)
+
+formatRPolyP :: Int -> Int -> Int -> String
+formatRPolyP y l s = printf "%d,%d %d,%d %d,%d %d,%d %d,%d" s y (s + l) y (s + l + 1) (y + 1) (s + l) (y + 2) s (y + 2)
+
+-- functions to connect contigs
 makePath :: Int -> Int -> Int -> Int -> S.AttributeValue
 makePath x1 y1 x2 y2 = mkPath $ do
   m x1 y1
@@ -56,19 +78,3 @@ connectCDSs [x] = connectCDSfromContigs x
 connectCDSs (x:xs) = do
                        connectCDSfromContigs x
                        connectCDSs xs
-
-makeTransform :: S.AttributeValue
-makeTransform = rotate 0
-
-makeCDS :: Int -> Int -> Int -> Bool -> S.Svg
-makeCDS y l s sense = S.polygon !
-  A.fill "#008d46" !
-  A.stroke "#000000" !
-  A.strokeWidth "0.2" !
-  (A.points $ toValue $ if sense then formatRPolyP y l s else formatLPolyP y l s)
-
-formatLPolyP :: Int -> Int -> Int -> String
-formatLPolyP y l s = printf "%d,%d %d,%d %d,%d %d,%d %d,%d" s (y + 1) (s + 1) y (s + l + 1) y (s + l + 1) (y + 2) (s + 1) (y + 2)
-
-formatRPolyP :: Int -> Int -> Int -> String
-formatRPolyP y l s = printf "%d,%d %d,%d %d,%d %d,%d %d,%d" s y (s + l) y (s + l + 1) (y + 1) (s + l) (y + 2) s (y + 2)
