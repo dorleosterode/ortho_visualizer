07a1d43829d7fc3300cbc4748f86542437db2e35
Dorle Osterode <9osterod@informatik.uni-hamburg.de>
2015-04-08 16:27:00 +0200

    added field Id and function to construct Genes from Strings

--- src/Gen.hs
+++ src/Gen.hs
@@ -1,21 +1,30 @@
-module Gen (Gen, Organism, Start, End, Direction, getOrganism, getStart, getEnd, getDirection,
-               setOrganism, setStart, setEnd, setDirection) where
+module Gen (Gen, Organism, Start, End, Direction, Id, getOrganism, getId, getStart, getEnd, getDirection,
+               setOrganism, setId, setStart, setEnd, setDirection,
+               convertToGen) where
 
 type Organism = String
 type Start = Int
 type End = Int
+type Id = Int
 type Direction = Bool
 
-data Gen = Gen Organism Start End Direction deriving (Eq, Show)
+data Gen = Gen Organism Id Start End Direction deriving (Eq, Show)
 
 -- functions to get information
-getOrganism (Gen org s e dir) = org
-getStart (Gen org s e dir) = s
-getEnd (Gen org s e dir) = e
-getDirection (Gen org s e dir) = dir
+getOrganism (Gen org id s e dir) = org
+getId (Gen org id s e dir) = id
+getStart (Gen org id s e dir) = s
+getEnd (Gen org id s e dir) = e
+getDirection (Gen org id s e dir) = dir
 
 -- functions to set the information new
-setOrganism (Gen org s e dir) n = (Gen n s e dir)
-setStart (Gen org s e dir) n = (Gen org n e dir)
-setEnd (Gen org s e dir) n = (Gen org s n dir)
-setDirection (Gen org s e dir) n = (Gen org s e n)
+setOrganism (Gen org id s e dir) n = (Gen n id s e dir)
+setId (Gen org id s e dir) n = (Gen org n s e dir)
+setStart (Gen org id s e dir) n = (Gen org id n e dir)
+setEnd (Gen org id s e dir) n = (Gen org id s n dir)
+setDirection (Gen org id s e dir) n = (Gen org id s e n)
+
+-- functions to convert gens from different strings to genes
+convertToGen :: String -> [String] -> Gen
+convertToGen org [c, id] = (Gen org (read id) 0 0 True)
+convertToGen org [_, _, s, e, "1"] = (Gen org 0 (read s) (read e) True)
