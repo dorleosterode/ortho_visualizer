49018dc6b8b456b8dddf7f80530020ab0c968c10
Dorle Osterode <9osterod@informatik.uni-hamburg.de>
2015-04-08 16:27:39 +0200

    source clean-up with hlint. more parsing, integrate the gene-datatype

--- src/parser_test.hs
+++ src/parser_test.hs
@@ -1,7 +1,10 @@
 import qualified Text.ParserCombinators.Parsec as P
 import Data.List.Split
+import Data.List
 import System.Exit
 import System.IO
+-- importing other self-written modules
+import Gen
 
 -- functions to parse the orthlogous-file
 orthomclFile :: P.GenParser Char st [[String]]
@@ -24,12 +27,28 @@ getTaxCode :: String -> String
 getTaxCode s = head $ splitOn "|" s
 
 orthologues :: [String] -> Bool
-orthologues xs = and [((length xs) == 3),
-                      ((getTaxCode (head (tail xs))) /= (getTaxCode (last xs)))]
+orthologues xs = length xs == 3 &&
+                 getTaxCode (head (tail xs)) /= getTaxCode (last xs)
 
-main = do
+-- function to convert [[String]] to [Gen]
+stringToGen :: String -> Gen
+stringToGen s = let
+                  [org, rest] = splitOn "|" s
+                  lst = splitOn "_" rest
+                in convertToGen org lst
+
+listToGenes :: [String] -> [Gen]
+listToGenes [x] = [stringToGen x]
+listToGenes (x : xs) = stringToGen x : listToGenes xs
+
+orthosToGenes :: [[String]] -> [Gen]
+orthosToGenes [x] = listToGenes $ tail x
+orthosToGenes (x : xs) = listToGenes (tail x) ++ orthosToGenes xs
+
+-- test the filter function
+testFilterOrthos = do
   all <- parseOrthologues "../data/groups.txt"
-  print $ filter orthologues all
+  print $ orthosToGenes $ filter orthologues all
 
 -- parser for gff
 gffFile :: P.GenParser Char st [[String]]
@@ -43,10 +62,22 @@ parseGff :: String -> IO [[String]]
 parseGff input = P.parseFromFile gffFile input >>= either report return
 
 -- filter function to get only CDS entries for the needed genes
-filterCDS :: [String] -> Bool
-filterCDS xs = ((length xs) >= 3) && ((xs !! 2) == "CDS")
+checkLineForEAndS :: Eq a => [a] -> [a] -> [[a]] -> Bool
+checkLineForEAndS s e = any (\x -> isInfixOf s x && isInfixOf e x)
+
+checkOrthologuesForCDS :: Eq a => [a] -> [a] -> [[[a]]] -> Bool
+checkOrthologuesForCDS s e = any (checkLineForEAndS s e)
+
+filterCDS :: [[String]] -> [String] -> Bool
+filterCDS orthos xs = (length xs >= 3) && ((xs !! 2) == "CDS")
+                      && checkOrthologuesForCDS (xs !! 3) (xs !! 4) orthos
 
 testFilterCDS = do
-  all <- parseGff "../data/NC_000913_e_coli.gff"
-  print $ length all
-  print $ length $ filter filterCDS all
+  geneInfo <- parseGff "../data/NC_012587.gff"
+  orthos <- parseOrthologues "../data/rhizobium_real.txt"
+  print $ length geneInfo
+  let filteredOrthos = filter orthologues orthos
+  print $ length $ filter (filterCDS filteredOrthos) geneInfo
+
+main =
+  testFilterCDS
